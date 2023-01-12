dccc06601f5314bce4167e43fdbedb2150da8314
rturquier <remi.turquier@ensae.fr>
2023-01-12 10:53:27 +0100

    🤸 Fit

--- numerical_experiment.py
+++ numerical_experiment.py
@@ -16,15 +16,27 @@ credit_df = (
     pd.read_csv(data_url, header=None)
     .rename(columns=column_renamer)
     .loc[:, ["A1", "A9", "A10", "A12", "A16"]]
-    .replace(regex={"\?": None, "[at\+]": True, "[bf\-]": False})
-    .convert_dtypes()
+    .replace(regex={"\?": None, "[at\+]": 1, "[bf\-]": -1})
+    .dropna()
 )
 
-y_data = credit_df.A1
-x_data = credit_df.loc[:, ["A9", "A10", "A12"]]
-s_data = credit_df.A16
+y_data = credit_df.A1.to_numpy()
+x_data = credit_df.loc[:, ["A9", "A10", "A12"]].to_numpy()
+s_data = credit_df.A16.to_numpy()
 
 x_train, x_test, y_train, y_test, s_train, s_test = (
     train_test_split(x_data, y_data, s_data,
                      train_size=0.80, shuffle=True)
 )
+
+fairness_notion = 'DDP'
+kernel = 'linear'
+verbose = True
+reg_beta = 0.0001
+
+linear_model_DDP = SearchFair(reg_beta=reg_beta,
+                              kernel=kernel,
+                              fairness_notion=fairness_notion,
+                              verbose=verbose,
+                              stop_criterion=0.01)
+linear_model_DDP.fit(x_train, y_train, s_train=s_train)
