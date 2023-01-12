42efa4d0d84c471a1c0ff865dedf801f2d5d5273
rturquier <remi.turquier@ensae.fr>
2023-01-12 10:59:35 +0100

    🖨️  Print classifier stats

--- numerical_experiment.py
+++ numerical_experiment.py
@@ -7,7 +7,31 @@ Authors: Robin Guillot, Grégoire Retourné, Rémi Turquier
 import pandas as pd
 from searchfair import SearchFair
 from sklearn.model_selection import train_test_split
+import examples.utils as ut
+import numpy as np
 
+# ------ Functions -------
+def print_clf_stats(model, x_train, x_test, y_train, y_test, s_train, s_test):
+    """Print classifier stats (function written by Lohaus et al.)"""
+    train_acc = ut.get_accuracy(np.sign(model.predict(x_train)), y_train)
+    test_acc = ut.get_accuracy(np.sign(model.predict(x_test)), y_test)
+    test_DDP, test_DEO = ut.compute_fairness_measures(model.predict(x_test),
+                                                      y_test,
+                                                      s_test)
+    train_DDP, train_DEO = ut.compute_fairness_measures(model.predict(x_train),
+                                                        y_train,
+                                                        s_train)
+
+    print(10*'-'+"Train"+10*'-')
+    print("Accuracy: %0.4f%%" % (train_acc * 100))
+    print("DDP: %0.4f%%" % (train_DDP * 100),
+          "DEO: %0.4f%%" % (train_DEO * 100))
+    print(10*'-'+"Test"+10*'-')
+    print("Accuracy: %0.4f%%" % (test_acc * 100))
+    print("DDP: %0.4f%%" % (test_DDP * 100),
+          "DEO: %0.4f%%" % (test_DEO * 100))
+
+# ------ Main code -------
 data_url = ("https://archive.ics.uci.edu/ml/"
             + "machine-learning-databases/credit-screening/crx.data")
 column_renamer = {index: "A" + str(index + 1) for index in range(16)}
@@ -40,3 +64,6 @@ linear_model_DDP = SearchFair(reg_beta=reg_beta,
                               verbose=verbose,
                               stop_criterion=0.01)
 linear_model_DDP.fit(x_train, y_train, s_train=s_train)
+
+print_clf_stats(linear_model_DDP,
+                x_train, x_test, y_train, y_test, s_train, s_test)
